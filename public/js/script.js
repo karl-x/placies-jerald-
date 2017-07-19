5e133cbc584182f89446533efa7cd073e27ca504
karl-x <jerald.yc.chua@gmail.com>
2017-07-19 10:43:58 +0800

    added spinner

--- public/js/script.js
+++ public/js/script.js
@@ -2,27 +2,24 @@ $(function () {
   const $placeSearch = $('#placeSearch')
   const apiUrl = 'https://maps.googleapis.com/maps/api/place/textsearch/json?'
   const imgUrl ='https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference='
-
-
   // <reference key>
   //   .reuslts[i].photos[i].photo_reference
 
   const keyword = ''
-  const apiKey = '&key=AIzaSyBUP2VHnTcSs1iGox2ah82YHXnEguXnFTs'
+  const apiKey = '&key=AIzaSyCulVEbmZslxhCcRVkOiQXmQ3PMG4ROBJ8'
+  // const $spinner = $('#spiner')
 
   $placeSearch.on('submit', function (e) {
+    $('.hidden').fadeIn()
     e.preventDefault()
 
     var keywordObj = $(this).serializeArray()
     var qString = `query=${keywordObj[0].value}`
 
-
     var finalUrl = `https://crossorigin.me/${apiUrl}${qString}${apiKey}`
 
-
-
-
     $.get(finalUrl).done(function (data) {
+
       $('h2').text('Your Search Results for '+ keyword + ':')
       var searchResults = data.results
       $('.results-list').empty()
@@ -36,11 +33,33 @@ $(function () {
         var $newLi = $('<li>')
         var $newImg = $('<img>')
         $newImg.attr('src', finalImgUrl)
+        $('.hidden').fadeOut()
         $newLi.text(searchResults[i].name)
         $newLi.append($newImg)
         $resultList.append($newLi)
       }
     })
+
+  //
+  //   // alternate method
+  //   function ajaxTextSearch (fimalUrl, keyword) {
+  //   $spinner.fadeIn()
+  //   $.get(finalUrl).done(function (data) {
+  //     $spinner.fadeOut()
+  //
+  //     var results = data.results
+  //     $keywordSearch.text(`Results for keyword:${keyword}`)
+  //
+  //     if ($searchResults.find('li').length)
+  //     $searchResults.html('')
+  //
+  //     results.forEach(function (place) {
+  //       var $newLi = $('<li>')
+  //       $newLi.text(place.name)
+  //       $searchResults.append($newLi)
+  //     })
+  //   })
+  // }
 
   })
 })
